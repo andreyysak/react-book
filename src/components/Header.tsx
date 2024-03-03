aeb4127ab1182532e7d590f48c0e3c8ef4e1a24c
Andriy Sak <andreyysak17@gmail.com>
2024-03-03 10:46:28 +0200

    make header layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { CiShoppingCart, CiDollar } from "react-icons/ci";
+import { FaBookOpen } from "react-icons/fa";
+
+
+export const Header = () => {
+  return (
+    <header className='flex justify-between items-center py-10 px-14 bg-white rounded-lg font-bold border-b-2'>
+      <figure>
+        <a 
+          href="www.google.com"
+          className='text-2xl flex items-center gap-4'
+        >
+          <FaBookOpen size={34}/>
+          React Book
+        </a>
+      </figure>
+
+      <ul className='flex list-none gap-7 bg-orangeCol text-white py-4 px-6 rounded-full'>
+        <li className='flex gap-2 pr-5 border-r-2'>
+          <label htmlFor="">0</label>
+          <CiDollar size={26} />
+        </li>
+        <li className='flex gap-2'>
+          <label htmlFor="">0</label>
+          <CiShoppingCart size={26} />
+        </li>
+      </ul>
+    </header>
+  )
+}
